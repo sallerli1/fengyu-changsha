7cb1b15f9532802f10ba8be171e9963790ce4888
dongyangyi123 <2589002087@qq.com>
2022-05-11 16:26:58 +0800

    feat: 丰羽计划代码 dyy

---
+++ dyy-w59061/src/components/KTableColumn.vue
@@ -0,0 +1,69 @@
+<template>
+  <th>
+    <!-- 展示列名 -->
+    <span>{{ label }}</span>
+    <!-- 实现升降排序 -->
+    <span @click="sort('desc')" v-if="sortable" class="isShow">⇃</span>
+    <span @click="sort('asc')" v-if="sortable"  class="isShow">↾</span>
+  </th>
+</template>
+<script>
+export default {
+  props: {
+    // 当前行的标签属性名
+    prop: {
+      type: String,
+      default: ''
+    },
+    // 当前行的标签属性文本
+    label: {
+      type: String,
+      default: ''
+    },
+    // 是否排序
+    sortable: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      // 实现排序功能
+      'asc-n': (a, b) => a - b,
+      'desc-n': (a, b) => b - a,
+      'asc-s': (a, b) => a.localeCompare(b),
+      'desc-s': (a, b) => b.localeCompare(a)
+    }
+  },
+  mounted () {
+    // 传回父组件，通知父组件把字符串加入顺序数组
+    this.$emit('table-cluomn', this.prop)
+  },
+  methods: {
+    sort (type) {
+      // 是否排序
+      if (this.sortable) {
+        // 对父父亲元素进行排序
+        this.$parent.$parent.pageDate.sort((a, b) => {
+          // 调用排序规则，传入排序方式asc、desc，然后判断数据类型进行拼接后调用方法实现排序
+          return this[type + this.getSortType(a[this.prop])](
+            a[this.prop],
+            b[this.prop]
+          )
+        })
+      }
+    },
+    // 处理排序类型
+    getSortType (val) {
+      // 返回标识串进行拼接
+      return typeof val === 'string' ? '-s' : '-n'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.isShow{
+    cursor: pointer;
+}
+</style>
---
+++ dyy-w59061/src/components/kTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="table-data">
+    <table border="1">
+      <tr>
+        <!-- 标题行插槽 用来接收 KtableColumn组件 -->
+        <slot></slot>
+      </tr>
+      <!-- 每一行的组件，根据index过来的数组生成行内数据 -->
+      <!--
+      model：传入每一个数组元素
+      prop：标题的顺序，用来传给下一级组件，来实现标题和列的对应
+     -->
+      <k-line
+        v-for="(item, index) in model"
+        :model="item"
+        :prop="prop"
+        :key="index"
+      ></k-line>
+    </table>
+  </div>
+</template>
+<script>
+import KLine from './Kline.vue'
+export default {
+  components: { KLine },
+  props: {
+    // 绑定的tabledata
+    model: {
+      type: Array,
+      default: () => []
+    },
+    // 标题顺序
+    prop: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
